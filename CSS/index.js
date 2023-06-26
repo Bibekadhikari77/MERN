bd178166695626b84174abc17836c42e171638e9
Bibekadhikari77 <127918639+Bibekadhikari77@users.noreply.github.com>
2023-06-26 05:18:47 -0700

    Add files via upload

---
+++ CSS/index.js
@@ -0,0 +1,7 @@
+let colors =['red', 'blue','green']
+
+console.log(colors)
+
+colors.splice(1,1)
+
+console.log(colors)
---
+++ CSS/script.js
@@ -0,0 +1,8 @@
+let colors =['red', 'blue','green']
+
+console.log(colors)
+
+colors.splice(1,1)
+
+console.log(colors)
+
